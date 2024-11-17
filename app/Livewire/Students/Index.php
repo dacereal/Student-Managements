6e2d1b0ec5076e9eb936ffaa04bf80edef9b30fb
dacereal <dapanascaryl@gmail.com>
2024-11-17 16:15:58 +0800

    Activity 6 - Implement loading and search

--- app/Livewire/Students/Index.php
+++ app/Livewire/Students/Index.php
@@ -1,17 +1,25 @@
 <?php
 namespace App\Livewire\Students;
 use App\Models\Student;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 use Livewire\WithoutUrlPagination;
 use Livewire\WithPagination;
 class Index extends Component
 {
+
+    public $search;
+
     use WithPagination, WithoutUrlPagination;
 
     public function render()
     {
+
+        $query = Student::query();
+        $query = $this->applySearch($query);
+
         return view('livewire.students.index', [
-            'students' => Student::paginate(10)
+            'students' => $query->paginate(10)
         ]);
     }
 
@@ -20,5 +28,11 @@ class Index extends Component
         $student->delete();
 
         flash()->info("Student deleted successfully!");
+    }
+
+    public function applySearch(Builder $query)
+    {
+        return $query->where('name', 'like', '%' . $this->search . '%')
+                    ->OrWhere('email', 'like', '%' . $this->search . '%');
     }
 }
